2a7eac81dedce847c374083ce7cda66016a33c28
Akaikenlol <67327997+Akaikenlol@users.noreply.github.com>
2020-07-05 15:54:30 +0630

    Add files via upload

---
+++ CursorLeft/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+namespace CursorLeft
+{
+    class Program
+    {
+        protected static int origRow;
+        protected static int origCol;
+        protected static void WriteAt(string s ,int x ,int y)
+        {
+        try
+        {
+            Console.SetCursorPosition(origCol+x,origRow+y);
+            Console.Write(s);
+        }
+        catch (ArgumentOutOfRangeException e)
+        {
+            Console.Clear();
+            Console.WriteLine(e.Message);
+        }
+        }
+        public static void Main (string[] args )
+        {
+            Console.Clear();
+            origRow=Console.CursorTop;
+            origCol=Console.CursorLeft;
+
+            WriteAt("+",0,0);
+            WriteAt("|",0,1);
+            WriteAt("|",0,2);
+            WriteAt("|",0,3);
+            WriteAt("+",0,4);
+
+            WriteAt("-",1,4);
+            WriteAt("-",2,4);
+            WriteAt("-",3,4);
+            WriteAt("+",4,4);
+
+            WriteAt("|",4,3);
+            WriteAt("|",4,2);
+            WriteAt("|",4,1);
+            WriteAt("+",4,0);
+
+            WriteAt("-",3,0);
+            WriteAt("-",2,0);
+            WriteAt("-",1,0);
+
+            WriteAt("All done!",0,6);
+            Console.WriteLine();
+        }
+    }
+}
